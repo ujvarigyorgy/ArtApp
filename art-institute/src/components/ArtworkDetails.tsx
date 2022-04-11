1e29b92c69cc70b685388a0450224f48d10e26a4
gyorgy <ujvari.gyorgy@gmail.com>
2022-04-11 11:44:54 +0300

    controls and dynamic link

--- art-institute/src/components/ArtworkDetails.tsx
+++ art-institute/src/components/ArtworkDetails.tsx
@@ -1,8 +1,45 @@
+import axios from 'axios';
+import {useEffect, useState} from 'react'
+import { useDispatch , useSelector } from 'react-redux';
+import { setProducts } from '../redux/actions/productActions';
+
 
 function ArtworkDetails() {
+    const dispatch = useDispatch()
+    const products:any = useSelector((state) => state)
+    const [loading, setLoading] = useState(false);
+    const [curresntPage, setCurrentpage] = useState(1);
+
+
+
+    useEffect(() => {
+        setLoading(true);
+        getArtist()
+        
+    },[curresntPage]);
+
+
+    const getArtist = async() => {
+        const response:any  = await axios 
+        .get(`https://api.artic.edu/api/v1/artworks?page=${curresntPage}&limit=25`)
+        .then((res:any)=>{
+            dispatch(setProducts(res.data.data))
+            console.log(products.allProducts.products[10].thumbnail,'products')
+        })
+        .catch((err:any) => {
+            console.log(err,'error')
+        })
+    }
   return (
     <div>
-     
+        {Object.keys(products.allProducts.products).map((i) => (
+            <div>
+                 <div key={i}>{products.allProducts.products[i].title}</div>
+                 <img src="" alt=""/>
+            </div>
+        ))}
+        <button onClick={()=> setCurrentpage(curresntPage+1)}>Previus</button>
+        <button onClick={()=> setCurrentpage(curresntPage+1)}>Next</button>
     </div>
   );
 }
--- art-institute/src/redux/reducers/productReducer.ts
+++ art-institute/src/redux/reducers/productReducer.ts
@@ -1,20 +1,14 @@
 import { ActionTypes } from "../constants/action-types"
 
 const initialState = {
-    products:[
-        {
-            id:1,
-            title: "Dipesh",
-            category: "programmer",
-        },
-    ]
+    products:[]
 }
 
 
 export const productReducer = (state = initialState, {type, payload}:any) => {
     switch(type) {
         case ActionTypes.SET_PRODUCTS:
-            return state;
+            return {...state, products:payload};
         default:
             return state;
     }
